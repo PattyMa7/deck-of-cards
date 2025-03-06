32104f1707baa4f69228ec2d1ef798e977856fde
PattyMaguina <p.maguina-aranda@mohawkcollege.ca>
2025-03-05 22:47:05 -0500

    Improving the app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Deck from "./components/Deck"; // Asegúrate de que este archivo existe
+import Deck from "./components/Deck";
 import "./styles.css";
 
-const App = () => {
-  return (
-    <div className="app">
-      <Deck />
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <Deck />
+  </div>
+);
 
 export default App;
